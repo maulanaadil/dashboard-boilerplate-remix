db39c5e3a32ed5a51d7dfcc79280c241995c1dc3
Maulana Adil <maulanaadil.latif@gmail.com>
2022-08-11 23:58:48 +0700

    add props className for heading & subhead

--- app/typography/heading/index.tsx
+++ app/typography/heading/index.tsx
@@ -10,39 +10,59 @@ import {
 } from './styles'
 import { cx } from '@emotion/css'
 
-const Heading = ({ type, children, ...otherProps }: HeadingProps) => {
+const Heading = ({
+    type,
+    children,
+    className,
+    ...otherProps
+}: HeadingProps) => {
     switch (type) {
         case 'h1':
             return (
-                <h1 className={cx(sHeading, sHeading1)} {...otherProps}>
+                <h1
+                    className={cx(sHeading, sHeading1, className)}
+                    {...otherProps}
+                >
                     {children}
                 </h1>
             )
 
         case 'h2':
             return (
-                <h2 className={cx(sHeading, sHeading2)} {...otherProps}>
+                <h2
+                    className={cx(sHeading, sHeading2, className)}
+                    {...otherProps}
+                >
                     {children}
                 </h2>
             )
 
         case 'h3':
             return (
-                <h3 className={cx(sHeading, sHeading3)} {...otherProps}>
+                <h3
+                    className={cx(sHeading, sHeading3, className)}
+                    {...otherProps}
+                >
                     {children}
                 </h3>
             )
 
         case 'h4':
             return (
-                <h4 className={cx(sHeading, sHeading4)} {...otherProps}>
+                <h4
+                    className={cx(sHeading, sHeading4, className)}
+                    {...otherProps}
+                >
                     {children}
                 </h4>
             )
 
         case 'h5':
             return (
-                <h5 className={cx(sHeading, sHeading5)} {...otherProps}>
+                <h5
+                    className={cx(sHeading, sHeading5, className)}
+                    {...otherProps}
+                >
                     {children}
                 </h5>
             )
--- app/typography/subhead/index.tsx
+++ app/typography/subhead/index.tsx
@@ -13,24 +13,40 @@ import {
 
 import type { SubheadProps } from './types'
 
-export const SubHead = ({ type, children }: SubheadProps) => {
+export const SubHead = ({ type, children, className }: SubheadProps) => {
     switch (type) {
         case 'subhead1':
-            return <p className={cx(sSubhead, sSubhead1)}>{children}</p>
+            return (
+                <p className={cx(sSubhead, sSubhead1, className)}>{children}</p>
+            )
         case 'subhead2':
-            return <p className={cx(sSubhead, sSubhead2)}>{children}</p>
+            return (
+                <p className={cx(sSubhead, sSubhead2, className)}>{children}</p>
+            )
         case 'subhead3':
-            return <p className={cx(sSubhead, sSubhead3)}>{children}</p>
+            return (
+                <p className={cx(sSubhead, sSubhead3, className)}>{children}</p>
+            )
         case 'subhead4':
-            return <p className={cx(sSubhead, sSubhead4)}>{children}</p>
+            return (
+                <p className={cx(sSubhead, sSubhead4, className)}>{children}</p>
+            )
         case 'subhead5':
-            return <p className={cx(sSubhead, sSubhead5)}>{children}</p>
+            return (
+                <p className={cx(sSubhead, sSubhead5, className)}>{children}</p>
+            )
         case 'subhead6':
-            return <p className={cx(sSubhead, sSubhead6)}>{children}</p>
+            return (
+                <p className={cx(sSubhead, sSubhead6, className)}>{children}</p>
+            )
         case 'subhead7':
-            return <p className={cx(sSubhead, sSubhead7)}>{children}</p>
+            return (
+                <p className={cx(sSubhead, sSubhead7, className)}>{children}</p>
+            )
         case 'subhead8':
-            return <p className={cx(sSubhead, sSubhead8)}>{children}</p>
+            return (
+                <p className={cx(sSubhead, sSubhead8, className)}>{children}</p>
+            )
 
         default:
             throw Error('please add type on subhead')
