e34f7ea7fabb8db5b35a84b1e598e4fba0de63de
Maulana Adil <maulanaadil.latif@gmail.com>
2022-08-14 14:11:08 +0700

    make dynamic username label

--- app/layout/header/styles.ts
+++ app/layout/header/styles.ts
@@ -8,16 +8,13 @@ export const sHeader = css`
     justify-content: space-between;
     align-items: center;
 
-    padding: 20px 24px;
     z-index: 3;
 
     background-color: ${color.primary.white1};
-
-    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1);
 `
 
 export const sHeaderTitle = css`
-    margin-bottom: 12px;
+    margin-bottom: 5px;
 `
 
 export const sHeaderSubtitle = css`
--- app/layout/header/types.ts
+++ app/layout/header/types.ts
@@ -3,4 +3,5 @@ import type { HTMLAttributes } from 'react'
 export type HeaderProps = {
     title: string
     subtitle: string
+    username: string
 } & HTMLAttributes<HTMLDivElement>
